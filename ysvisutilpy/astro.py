3b651c8db4236ff75cfcfa5f73e9a261143b86d7
ysBach <dbstn95@gmail.com>
2019-12-01 15:12:41 +0900

    add zscale option to norm_imshow

--- ysvisutilpy/astro.py
+++ ysvisutilpy/astro.py
@@ -22,9 +22,14 @@ def zimshow(ax, image, stretch=LinearStretch(), cmap=None, origin='lower',
 def norm_imshow(ax, data, origin='lower', stretch='linear', power=1.0,
                 asinh_a=0.1, min_cut=None, max_cut=None, min_percent=None,
                 max_percent=None, percent=None, clip=True, log_a=1000,
-                **kwargs):
+                zscale=False, **kwargs):
     """ Do normalization and do imshow
     """
+    if zscale:
+        zs = ImageNormalize(data, interval=ZScaleInterval())
+        min_cut = zs.vmin
+        max_cut = zs.vmax
+
     norm = simple_norm(data, stretch=stretch, power=power, asinh_a=asinh_a,
                        min_cut=min_cut, max_cut=max_cut,
                        min_percent=min_percent, max_percent=max_percent,
