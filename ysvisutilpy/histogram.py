d043dc33245215895818fba5703fb3cff4276c5d
ysBach <dbstn95@gmail.com>
2020-06-16 12:39:05 +0900

    internal api change

--- ysvisutilpy/histogram.py
+++ ysvisutilpy/histogram.py
@@ -12,12 +12,12 @@ def hist(data, bins, range=None, normed=None, weights=None, density=None):
     Example
     -------
     >>> fig, ax = plt.subplots(1, 1)
-    >>> pos, hist, width, edges = hist(data, np.arange(-3, 3, 0.5))
-    >>> ax.bar(pos, hist, width, fill=False, edgecolor='k')
+    >>> pos, n, width, edges = hist(data, np.arange(-3, 3, 0.5))
+    >>> ax.bar(pos, n, width, fill=False, edgecolor='k')
     '''
-    hist, edges = np.histogram(data, bins=bins, range=range,
-                               normed=normed, weights=weights,
-                               density=density)
+    n, edges = np.histogram(data, bins=bins, range=range,
+                            normed=normed, weights=weights,
+                            density=density)
     pos = (edges[1:] + edges[:-1])/2
     width = np.ediff1d(edges)
-    return pos, hist, width, edges
+    return pos, n, width, edges
